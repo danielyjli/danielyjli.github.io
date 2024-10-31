fcdf1cc4de55829a90a4a4f6ef4dd06f20baa61c
Ladykiller711 <133202557+danielyjli@users.noreply.github.com>
2024-10-31 08:32:00 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,22 @@
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections= document.querySelectorALl('section[id]')
 
+const scrollActive =()=>{
+    const scrollDown=window.scrollY
+
+    sections.forEach(Current =>{
+        const sectionHeight =current.offsetHeight,
+        sectionTop =current.offsetTop - 58
+        sectionId= current.getAttribute('id'),
+        sectionsClass =document.querySelector('.nav__menu a[href*=' + sectionId +']')
+        if(scrollDown>sectionTop &&scrollDown <= sectionTop +sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }
+            sectionsClass.classList.remove('active-link')
+    })
+}
+window.addEventListener('scroll',scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
