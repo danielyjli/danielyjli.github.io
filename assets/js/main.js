29b025f5375a1902a516b9ae9f4e4c9b800c2531
Ladykiller711 <133202557+danielyjli@users.noreply.github.com>
2024-10-31 08:54:31 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ const scrollActive =()=>{
         const sectionHeight =current.offsetHeight,
         sectionTop =current.offsetTop - 58
         sectionId= current.getAttribute('id'),
-        sectionsClass =document.querySelector('.nav__menu a[href*=' + sectionId +']')
+        sectionsClass =document.querySelector('.nav__list a[href*=' + sectionId +']')
         if(scrollDown>sectionTop &&scrollDown <= sectionTop +sectionHeight){
             sectionsClass.classList.add('active-link')
         }
